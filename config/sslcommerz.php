24a066fa7ab4193f419de0cfacdf11f30e6e2937
Prabal Mallick <prabalsslw@gmail.com>
2019-07-16 15:34:46 +0600

    Updating codebase for integrating SSLCommerz API v4

---
+++ config/sslcommerz.php
@@ -0,0 +1,26 @@
+<?php
+
+// SSLCommerz configuration
+
+return [
+    'projectPath' => env('PROJECT_PATH'),
+    // For Sandbox, use "https://sandbox.sslcommerz.com"
+    // For Live, use "https://securepay.sslcommerz.com"
+    'apiDomain' => env("API_DOMAIN_URL"),
+    'apiCredentials' => [
+        'store_id' => env("STORE_ID"),
+        'store_password' => env("STORE_PASSWORD"),
+    ],
+    'apiUrl' => [
+        'make_payment' => "/gwprocess/v4/api.php",
+        'transaction_status' => "/validator/api/merchantTransIDvalidationAPI.php",
+        'order_validate' => "/validator/api/validationserverAPI.php",
+        'refund_payment' => "/validator/api/merchantTransIDvalidationAPI.php",
+        'refund_status' => "/validator/api/merchantTransIDvalidationAPI.php",
+    ],
+    'connect_from_localhost' => env("IS_LOCALHOST", true), // For Sandbox, use "true", For Live, use "false"
+    'success_url' => '/success',
+    'failed_url' => '/fail',
+    'cancel_url' => '/cancel',
+    'ipn_url' => '/ipn',
+];
