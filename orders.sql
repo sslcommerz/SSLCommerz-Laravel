a751d19fe3dfabd9fdc16799ed58399a117e2805
rkbi <rakibul.islam@sslwireless.com>
2021-06-18 18:46:54 +0600

    Update orders.sql

--- orders.sql
+++ orders.sql
@@ -1,5 +1,5 @@
 CREATE TABLE `orders` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
   `name` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
   `email` varchar(30) COLLATE utf8_unicode_ci DEFAULT NULL,
   `phone` varchar(20) COLLATE utf8_unicode_ci DEFAULT NULL,
@@ -9,4 +9,3 @@ CREATE TABLE `orders` (
   `transaction_id` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
   `currency` varchar(20) COLLATE utf8_unicode_ci DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
